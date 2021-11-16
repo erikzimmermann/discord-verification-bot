8f9f4f95432eaf9871ecc884dd12b78c5dd46cd7
Erik Zimmermann <mail@erikzimmermann.de>
2021-11-16 13:30:28 +0100

    Fix typo

--- system/discord_utils.py
+++ system/discord_utils.py
@@ -16,14 +16,14 @@ class Discord:
         self.working_queue: list = []
 
     async def fetch(self) -> None:
-        self.guild = await self.client.fetch_guild(self.client["discord"]["guild_id"])
+        self.guild = await self.client.fetch_guild(self.config["discord"]["guild_id"])
         self.premium_role = await self.__fetch_role__()
-        self.admin_channel = await self.client.fetch_channel(self.client["discord"]["admin_channel"])
+        self.admin_channel = await self.client.fetch_channel(self.config["discord"]["admin_channel"])
 
     # Fetches the premium role with the premium_id from the config.json.
     async def __fetch_role__(self) -> Optional[discord.Role]:
         roles = await self.guild.fetch_roles()
         for role in roles:
-            if role.id == self.client["discord"]["premium_role"]:
+            if role.id == self.config["discord"]["premium_role"]:
                 return role
         return None
